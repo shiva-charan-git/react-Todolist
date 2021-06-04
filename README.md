59bb8f2116f74c39f5c3b87596dc2158ec684f0e
shiva-charan-git <shivacharan021@gmail.com>
2021-06-04 15:11:59 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-jmzpib
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-jmzpib)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+import TodoApp from './TodoApp.js'
+
+export default function App() {
+  return (
+    <div>
+     <TodoApp />
+    </div>
+  );
+}
